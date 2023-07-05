aedd2426b6578c4ea7b0589aaf272dcaae59a4b0
LuisGil11 <Giluis2000@gmail.com>
2023-07-05 17:37:32 -0400

    Back y Front en la verga pero sirve todo con el log in :)

--- backend/venv/prueba.py
+++ backend/venv/prueba.py
@@ -1,14 +1,4 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
 import cx_Oracle
-import json
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'Hello from the backend!'}
-    return jsonify(data)
 
 # Configuración de la conexión
 username = 'C##REDES'  # Nombre de usuario de la base de datos
@@ -25,11 +15,19 @@ try:
     connection = cx_Oracle.connect(username, password, dsn)
     print("conexion con oracle")
     cursor = connection.cursor()
+
+    sql_select = "SELECT * FROM CLIENTE"
+
+    cursor.execute(sql_select)
+    # Obtener todas las filas de resultados
+    rows = cursor.fetchall()
+
+    # Mostrar los resultados
     
+    for row in rows:
+        print(row)
+
     # Cerrar la conexión
     connection.close()
 except cx_Oracle.Error as error:
     print("Error al conectar a Oracle: ", error)
-
-if __name__ == '__main__':
-    app.run()
